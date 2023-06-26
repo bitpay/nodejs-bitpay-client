602ed6381e8258ef7b3f0c08c2f92f0261fb02e5
Marcin Warzybok <m.warzybok@sumoheavy.com>
2023-06-26 16:26:20 +0200

    add refundRequestEid

--- src/Model/Settlement/RefundInfo.ts
+++ src/Model/Settlement/RefundInfo.ts
@@ -1,12 +1,14 @@
 export interface RefundInfoInterface {
   supportRequest: string | null;
   currency: string | null;
+  refundRequestEid: string | null;
   amounts: [];
 }
 
 export class RefundInfo implements RefundInfoInterface {
   supportRequest: string | null;
   currency: string | null;
+  refundRequestEid: string | null;
   amounts: [];
 
   public constructor() {}
