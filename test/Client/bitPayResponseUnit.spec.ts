4c2892f10d14c0f7d25606f71d63e5a33f9ba645
Marcin Warzybok <m.warzybok@sumoheavy.com>
2024-02-26 16:43:03 +0100

    SP-874 Support "errors" array

--- test/Client/bitPayResponseUnit.spec.ts
+++ test/Client/bitPayResponseUnit.spec.ts
@@ -1,13 +1,18 @@
-import {BitPayResponseParser} from "../../src/util/BitPayResponseParser";
+import { BitPayResponseParser } from '../../src/util/BitPayResponseParser';
 
 describe('BitPayResponse', function () {
   it('should handle multiple errors', async () => {
-    const bitPayResponseParser = new BitPayResponseParser()
-    
+    const bitPayResponseParser = new BitPayResponseParser();
+
     try {
-      await bitPayResponseParser.getJsonDataFromJsonResponse({"errors":[{"error":"Missing required parameter.","param":"price"},{"error":"Missing required parameter.","param":"currency"}]});
+      await bitPayResponseParser.getJsonDataFromJsonResponse({
+        errors: [
+          { error: 'Missing required parameter.', param: 'price' },
+          { error: 'Missing required parameter.', param: 'currency' }
+        ]
+      });
     } catch (e: any) {
       expect(e.message).toBe('Missing required parameter price. Missing required parameter currency.');
     }
-  })
-})
+  });
+});
