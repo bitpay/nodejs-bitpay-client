f495faea306c19e2b91a7f691867fbaa1bb4dbb4
Marcin Warzybok <m.warzybok@sumoheavy.com>
2024-02-26 16:40:41 +0100

    SP-874 Support "errors" array

--- src/util/BitPayResponseParser.ts
+++ src/util/BitPayResponseParser.ts
@@ -28,6 +28,25 @@ export class BitPayResponseParser {
         BitPayExceptionProvider.throwApiExceptionWithMessage(responseObj['error'] ?? null, responseObj['code'] ?? null);
       }
 
+      if (Object.prototype.hasOwnProperty.call(responseObj, 'errors')) {
+        let result = '';
+        responseObj['errors'].forEach((error, index) => {
+          if (index !== 0) {
+            result += ' ';
+          }
+
+          const errorText = error['error'].endsWith('.') ? error['error'].slice(0, -1) : error['error'];
+          const param = Object.prototype.hasOwnProperty.call(error, 'param') ? error['param'] : '';
+          result += `${errorText} ${param}.`;
+
+          if (!result.endsWith('.')) {
+            result += '.';
+          }
+        });
+
+        BitPayExceptionProvider.throwApiExceptionWithMessage(result, responseObj['code'] ?? null);
+      }
+
       if (Object.prototype.hasOwnProperty.call(responseObj, 'success')) {
         return JSON.stringify(responseObj['success']);
       }
---
+++ test/Client/bitPayResponseUnit.spec.ts
@@ -0,0 +1,13 @@
+import {BitPayResponseParser} from "../../src/util/BitPayResponseParser";
+
+describe('BitPayResponse', function () {
+  it('should handle multiple errors', async () => {
+    const bitPayResponseParser = new BitPayResponseParser()
+    
+    try {
+      await bitPayResponseParser.getJsonDataFromJsonResponse({"errors":[{"error":"Missing required parameter.","param":"price"},{"error":"Missing required parameter.","param":"currency"}]});
+    } catch (e: any) {
+      expect(e.message).toBe('Missing required parameter price. Missing required parameter currency.');
+    }
+  })
+})
