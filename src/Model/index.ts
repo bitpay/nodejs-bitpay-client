c69d1d95b858f4185c8cf609f3eed9ebd6c0748b
Damian Matuszczak <dmatuszczak@itx.com>
2025-01-15 13:43:58 +0100

    SP-1135 Add exports to Node.js SDK

--- src/Model/index.ts
+++ src/Model/index.ts
@@ -20,26 +20,96 @@ import { Bill, BillInterface } from './Bill/Bill';
 import { Item as BillItem } from './Bill/Item';
 import { LedgerInterface } from './Ledger/Ledger';
 import { LedgerEntryInterface } from './Ledger/LedgerEntry';
+import { BuyerFieldsInterface, BuyerFields } from './Ledger/BuyerFields';
 import { PayoutRecipient, PayoutRecipientInterface } from './Payout/PayoutRecipient';
 import { PayoutRecipients, PayoutRecipientsInterface } from './Payout/PayoutRecipients';
 import { Payout, PayoutInterface } from './Payout/Payout';
 import { PayoutGroupInterface } from './Payout/PayoutGroup';
+import { CurrencyInterface, Currency } from './Currency/Currency';
+import { PayoutTransaction, PayoutTransactionInterface } from './Payout/PayoutTransaction';
+import { PayoutGroupFailed, PayoutGroupFailedInterface } from './Payout/PayoutGroupFailed';
+import { Buyer, BuyerInterface } from './Invoice/Buyer';
+import { InvoiceBuyerProvidedInfo } from './Invoice/InvoiceBuyerProvidedInfo';
+import { InvoiceEventTokenInterface } from './Invoice/InvoiceEventToken';
+import { InvoiceRefundAddresses } from './Invoice/InvoiceRefundAddresses';
+import { InvoiceTransaction } from './Invoice/InvoiceTransaction';
+import { InvoiceUniversalCodes } from './Invoice/InvoiceUniversalCodes';
+import { MinerFeesItem } from './Invoice/MinerFeesItem';
+import { RefundInterface, Refund } from './Invoice/Refund';
+import { RefundInfo } from './Invoice/RefundInfo';
+import { Shopper } from './Invoice/Shopper';
+import { SupportedTransactionCurrency } from './Invoice/SupportedTransactionCurrency';
+import { InvoiceDataInterface, InvoiceData } from './Settlement/InvoiceData';
+import { PayoutInfoInterface, PayoutInfo } from './Settlement/PayoutInfo';
+import { RefundInfoInterface } from './Settlement/RefundInfo';
+import { SettlementInterface, Settlement } from './Settlement/Settlement';
+import { SettlementLedgerEntryInterface, SettlementLedgerEntry } from './Settlement/SettlementLedgerEntry';
+import { WithHoldingsInterface, WithHoldings } from './Settlement/WithHoldings';
+import { CurrenciesInterface, Currencies } from './Wallet/Currencies';
+import { CurrencyQr, CurrencyQrInterface } from './Wallet/CurrencyQr';
+import { Wallet, WalletInterface } from './Wallet/Wallet';
+import { InvoiceWebhook } from './Webhook/InvoiceWebhook';
+import { PayoutWebhookInterface } from './Webhook/PayoutWebhook';
+import { RefundWebhook } from './Webhook/RefundWebhook';
+import { InvoiceWebhookBuyerFieldsInterface } from './Webhook/InvoiceWebhookBuyerFields';
 
 export {
   Invoice,
   InvoiceInterface,
+  InvoiceData,
+  InvoiceDataInterface,
+  InvoiceBuyerProvidedInfo,
+  InvoiceEventTokenInterface,
+  InvoiceRefundAddresses,
+  InvoiceTransaction,
+  InvoiceUniversalCodes,
+  InvoiceWebhook,
+  InvoiceWebhookBuyerFieldsInterface,
   RateInterface,
   Rates,
   Bill,
   BillInterface,
   BillItem,
+  BuyerFields,
+  BuyerFieldsInterface,
+  Buyer,
+  BuyerInterface,
+  Currency,
+  CurrencyInterface,
+  Currencies,
+  CurrenciesInterface,
+  CurrencyQr,
+  CurrencyQrInterface,
   LedgerInterface,
   LedgerEntryInterface,
+  MinerFeesItem,
   PayoutRecipient,
   PayoutRecipientInterface,
   PayoutRecipients,
   PayoutRecipientsInterface,
   Payout,
   PayoutInterface,
-  PayoutGroupInterface
+  PayoutInfo,
+  PayoutInfoInterface,
+  PayoutGroupInterface,
+  PayoutTransaction,
+  PayoutTransactionInterface,
+  PayoutGroupFailed,
+  PayoutGroupFailedInterface,
+  PayoutWebhookInterface,
+  Refund,
+  RefundInterface,
+  RefundInfo,
+  RefundInfoInterface,
+  RefundWebhook,
+  Shopper,
+  Settlement,
+  SettlementInterface,
+  SettlementLedgerEntry,
+  SettlementLedgerEntryInterface,
+  SupportedTransactionCurrency,
+  WithHoldings,
+  WithHoldingsInterface,
+  Wallet,
+  WalletInterface
 };
