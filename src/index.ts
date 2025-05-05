3cc798f86d98fbe3adbba99e3b668fb885a2b6c0
Robert Brodie <r.brodie@sumoheavy.com>
2025-05-05 15:15:25 -0400

    #134 Add export for Environment

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { Config } from './Config';
 import { Currency } from './Currency';
 import { Facade } from './Facade';
 import * as Env from './Env';
-import * as Environment from './Env';
+import * as Environment from './Environment';
 import * as Invoice from './Model/Invoice/Invoice';
 import * as InvoiceStatus from './Model/Invoice/InvoiceStatus';
 import * as RefundStatus from './Model/Invoice/RefundStatus';
